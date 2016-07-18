f99562c22603f858396201e113ab6ad0ae7dc32b
Japan Parikh <parikh.japan30@gmail.com>
2016-07-17 18:57:50 -0700

    Add api to get game

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from google.appengine.ext import ndb
 from models import User, Game
 from models import UserForm, GameForm, UserForms, GameForms
 from models import StringMessage, MakeMoveForm, NewGameForm
+from utils import get_by_urlsafe
 
 
 NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
@@ -18,7 +19,7 @@ USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
 @endpoints.api(name='battle_ship', version='v1')
 class BattleShipAPI(remote.Service):
     """Game API"""
-    @endpoints.method(USER_REQUEST, StringMessage, name='createUser',
+    @endpoints.method(USER_REQUEST, StringMessage, name='create_user',
                       path='user', http_method='POST')
     def create_user(self, request):
         """Creates a user"""
@@ -28,6 +29,17 @@ class BattleShipAPI(remote.Service):
         user = User(name=request.user_name, email=request.email)
         user.put()
         return user
+
+    @endpoints.method(GET_GAME_REQUEST, GameForm, name='get_game',
+                      path='game/{urlsafe_game_key}', http_method='GET')
+    def get_game(self, request):
+        """Return the current game state"""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            return game.to_form()
+        else:
+            raise endpoints.NotFoundException('Game not found!')
+
 
 
 api = endpoints.api_server([BattleShipAPI])
